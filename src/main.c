e83deb640290dda92cc114f2dd05d0f89e4a77d3
Christopher Broome <ChrisBroome7@gmail.com>
2013-07-25 01:24:36 -0400

    main: print command line arguments

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
+
 #include "hello.h"
 
 int main(int argc, char* argv[]) {
   hello();
+  for(int i = 0; i < argc; ++i) {
+    printf("%d: %s\n", i, argv[i]);
+  }
   return 0;
 }
